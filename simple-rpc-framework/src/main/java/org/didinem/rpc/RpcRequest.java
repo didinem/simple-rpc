b9171ff853846bbc8e6ec34d0ae975eba0b350e9
didinem <didinem@sina.com>
2017-04-10 20:44:15 +0800

    init

---
+++ simple-rpc-framework/src/main/java/org/didinem/rpc/RpcRequest.java
@@ -0,0 +1,14 @@
+package org.didinem.rpc;
+
+/**
+ * Created by zhongzhengmin on 2017/4/10.
+ */
+public class RpcRequest {
+
+    private String serviceKey;
+
+    private String methodName;
+
+    private Object[] params;
+
+}
--- simple-rpc-framework/src/main/java/org/didinem/rpc/server/RpcServerChannelHandler.java
+++ simple-rpc-framework/src/main/java/org/didinem/rpc/server/RpcServerChannelHandler.java
@@ -2,6 +2,7 @@ package org.didinem.rpc.server;
 
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+import org.didinem.rpc.RpcRequest;
 
 /**
  * Created by didinem on 4/9/2017.
@@ -10,6 +11,9 @@ public class RpcServerChannelHandler extends ChannelHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        RpcRequest rpcRequest = (RpcRequest) msg;
+
+
         super.channelRead(ctx, msg);
     }
 }
